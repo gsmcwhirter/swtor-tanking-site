1f7b3fc5727f93a242bf8c925ab144d3f9a150b2
Gregory McWhirter <greg@hallofkvasir.org>
2013-05-21 14:47:39 -0700

    Updates to the optimizer to make it work, and adding the 'about' section

--- js/index.js
+++ js/index.js
@@ -1,6 +1,124 @@
-var superagent = require("superagent")
-  , page = require("page.js")
+var request = require("superagent")
+  , page = require("page")
   , loading = require("loading-lock")
   , dom = require("dom")
+  , json = require("json")
+  , each = require("each")
+  , locker = loading(dom("#actual_results").els[0], {size: 80})
   ;
   
+function isIntNumber(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n) && n % 1 === 0;
+}
+
+function formatIntNumber(n) {
+  return Math.floor(parseFloat(n));
+}  
+
+function clearPage(){
+  dom("#main, #about").each(function (item){
+    item.els[0].style.display = 'none';
+  });
+}
+
+function showIndex(){
+  clearPage();
+  dom("#main").els[0].style.display = '';
+}
+
+function showAssumptions(){
+  clearPage();
+  dom("#about").els[0].style.display = '';
+}
+  
+module.exports = function run(){
+  page("", showIndex);
+  page("/", showIndex);
+  page("/about", showAssumptions);
+  page();
+
+  //TODO: integrate relics
+
+  var numberFields = [
+    "#defRating"
+  , "#shieldRating"
+  , "#absorbRating"
+  , "#armorRating"
+  ];
+
+  each(numberFields, function (field){
+    dom(field).on('keyup', validateNumber);
+    dom(field).on('change', validateNumber);
+    dom(field).on('blur', validateNumber);
+  });
+  
+  dom("#process").on('click', function (e){
+    e.preventDefault();
+    
+    locker.lock();
+    
+    var klass = dom("#class").els[0];
+    var stim = dom("#stim").els[0];
+        
+    var data = {
+      'class': klass.options[klass.selectedIndex].value
+    , 'stim': stim.options[stim.selectedIndex].value
+    , 'defRating': formatIntNumber(dom("#defRating").els[0].value)
+    , 'shieldRating': formatIntNumber(dom("#shieldRating").els[0].value)
+    , 'absorbRating': formatIntNumber(dom("#absorbRating").els[0].value)
+    , 'armorRating': formatIntNumber(dom("#armorRating").els[0].value)
+    };
+    
+    request.post("/api/optimize")
+           .send(data)
+           .set('Accept', 'application/json')
+           .end(function (err, res){
+              locker.unlock();
+              if (err || (res.status != 200 && res.status != 304) || res.error){
+                //TODO: error handling
+              }
+              else {
+                //TODO: real display of results
+                var results = json.parse(res.text).result;
+                console.log(results);
+                
+                var ddiff = results.defRating - data.defRating;
+                var dclass = ddiff >= 0 ? 'add' : 'sub';
+                ddiff = "" + (ddiff >= 0 ? "+" : "") + ddiff;
+                
+                var sdiff = results.shieldRating - data.shieldRating;
+                var sclass = sdiff >= 0 ? 'add' : 'sub';
+                sdiff = "" + (sdiff >= 0 ? "+" : "") + sdiff;
+                
+                var adiff = results.absorbRating - data.absorbRating;
+                var aclass = adiff >= 0 ? 'add' : 'sub';
+                adiff = "" + (adiff >= 0 ? "+" : "") + adiff;
+                
+                dom("#actual_results #defRatingValue").els[0].innerHTML = results.defRating + " (<span class='" + dclass + "'>" + ddiff + "</span>)";
+                dom("#actual_results #shieldRatingValue").els[0].innerHTML = results.shieldRating + " (<span class='" + sclass + "'>" + sdiff + "</span>)";
+                dom("#actual_results #absorbRatingValue").els[0].innerHTML = results.absorbRating + " (<span class='" + aclass + "'>" + adiff + "</span>)";
+                
+                dom("#actual_results #defPctNS").els[0].innerHTML = Math.floor(results.defPctNBNS * 10000) / 100 + "%";
+                dom("#actual_results #defPctS").els[0].innerHTML = Math.floor(results.defPctNB * 10000) / 100 + "%";
+                dom("#actual_results #shieldPct").els[0].innerHTML = Math.floor(results.shieldPctNB * 10000) / 100 + "%";
+                dom("#actual_results #absorbPct").els[0].innerHTML = Math.floor(results.absorbPctNB * 10000) / 100 + "%";
+              }
+           });
+  });
+  
+  dom("#clear").on('click', function (e){
+    e.preventDefault();
+    each(numberFields, function (field){
+      dom(field).els[0].value = 0;
+    });
+    
+    dom("#class").els[0].selectedIndex = 0;
+    dom("#stim").els[0].selectedIndex = 0;
+  });
+}
+
+function validateNumber(e){
+  if (!isIntNumber(e.target.value) && e.target.value !== ''){
+    e.target.value = formatIntNumber(e.target.value);
+  }
+}
--- server.js
+++ server.js
@@ -6,12 +6,17 @@ var cluster = require('cluster')
   , express = require('express')
   , join = require('path').join
   , fs = require('fs')
+  , sto = require('swtor-tanking')
   ;
   
 var procs = process.env.procs || 1
   , port = process.env.port || 3001
   , host = process.env.host || '127.0.0.1'
   ;
+  
+function isIntNumber(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n) && n % 1 === 0;
+}
   
 if (cluster.isMaster) {
   //fork workers
@@ -30,15 +35,13 @@ else {
 
   app.use(express.favicon());
   app.use(express.logger());
-
+  app.use(express.bodyParser());
+  
+  app.enable('trust proxy');
   app.enable('strict routing');
 
   // routes
-
-  /**
-   * GET /* as a file if it exists.
-   */
-
+  
   app.get('/:file(*)', function(req, res, next){
     var file = req.params.file;
     if (!file) return next();
@@ -50,14 +53,80 @@ else {
     });
   });
 
-  /**
-   * GET /* as index.html
-   */
-
+  // redir / to index.html
   app.get('/*', function(req, res){
     var name = req.params.example;
     res.sendfile(join(__dirname, "static", name, 'index.html'));
   });
+  
+  app.post('/api/optimize', function (req, res){
+    var data_keys = [ 'class'
+                    , 'stim'
+                    ]
+      , nume_keys = [ 'defRating'
+                    , 'shieldRating'
+                    , 'absorbRating'
+                    , 'armorRating'
+                    ]
+      ;
+    
+    var error_msgs = [];
+    
+    data_keys.concat(nume_keys).forEach(function (key){
+      if (!(req.body[key] || req.body[key] === 0)){
+        error_msgs.push("Expected data '" + key + "' was not present.");
+      }
+    });
+    
+    if (req.body['class'] && ['shadow', 'guardian', 'vanguard'].indexOf(req.body['class']) === -1){
+      error_msgs.push("Class value was not expected.");
+    }
+    
+    if (req.body['stim'] && ['proto_nano', 'reuse_nano', 'none'].indexOf(req.body['stim']) === -1){
+      error_msgs.push("Stim value was not expected.");
+    }
+    
+    //TODO: add relic support here
+    
+    nume_keys.forEach(function (key){
+      if (req.body[key] && !isIntNumber(req.body[key]) && parseFloat(req.body[key]) > 0){
+        error_msgs.push(key + " must be a non-negative integer value.");
+      }
+    });
+    
+    
+    res.set('Content-type', 'application/json')
+    if (error_msgs.length) {
+      //errror in parameters
+      res.send({error: error_msgs});
+    }
+    else {
+      //try to optimize
+      var statBudget = 0;
+      statBudget += parseFloat(req.body['defRating']);
+      statBudget += parseFloat(req.body['shieldRating']);
+      statBudget += parseFloat(req.body['absorbRating']);
+      
+      var stimValue = 0;
+      if (req.body['stim'] === "proto_nano"){
+        stimValue = 70;
+      }
+      else if (req.body['stim'] === "reuse_nano"){
+        stimValue = 63;
+      }
+      
+      sto.optimizer.optimize(sto.otherData, sto.classData[req.body['class']], statBudget, parseFloat(req.body['armorRating']), stimValue, function (err, result){
+        if (err){
+          res.send({error: err});
+        }
+        else {
+          res.send({result: result});
+        }
+      });
+    }
+  });
+  
+  //app.use("/", express.static(join(__dirname, "static")));
 
   if (host && host !== 'INADDR_ANY'){
     app.listen(port, host);  
